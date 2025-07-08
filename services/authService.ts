7ed59383e6f14aa3bc0ff42d5688ddfba8eaf9b2
Diego Pineda <zentoo31@gmail.com>
2025-07-07 21:41:02 -0500

    Refactor login method to improve error handling and maintainability

--- services/authService.ts
+++ services/authService.ts
@@ -1,23 +1,31 @@
 export class AuthService {
     private URL: string = 'http://10.0.2.2/api/auth';
     async login(email: string, password: string): Promise<any> {
-        const response = await fetch(`${this.URL}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
+        try {
+            const response = await fetch(`${this.URL}/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            const errorMessage = errorData.message || 'Login failed';
-            throw new Error(errorMessage);
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData.message || 'Login failed';
+                throw new Error(errorMessage);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error('Error during login:', error.message);
+            } else {
+                console.error('Error during login:', error);
+            }
+            throw error;
         }
-
-        const data = await response.json();
-        return data;
-
     }
     async register(email: string, password: string): Promise<any> {
         try {
