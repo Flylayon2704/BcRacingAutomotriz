0c32afe1da1f4ea3e9c7e5ea6d884afd337e8b78
Flylayon2704 <lyleon2704@gmail.com>
2025-07-08 23:06:59 -0500

    width

--- app/stylesProduct.ts
+++ app/stylesProduct.ts
@@ -1,4 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
